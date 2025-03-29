824a74c56563915fcddfbd2d4d110246412d80fe
ganle8080 <siburnjuly@gmail.com>
2025-03-29 20:54:00 +0800

    add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ganle8080/easy-sql
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	Helloworld()
+}
+
+func Helloworld() {
+	fmt.Println("Hello World!")
+}
