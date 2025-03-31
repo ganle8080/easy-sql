e4f666f35674b5dd53e2e01302f401da1247f98e
subingjie <kf.subingjie@cloudnineinfo.com>
2025-03-31 11:10:08 +0800

    add

--- src/load.go
+++ src/load.go
@@ -5,41 +5,6 @@ import (
 	"os"
 )
 
-// SearchSchema 通用查询schema结构体
-type SearchSchema struct {
-	TableName  string      `json:"table_name"`
-	Columns    []Column    `json:"columns"`
-	Joins      []Join      `json:"joins"`
-	Conditions []Condition `json:"conditions"`
-	Orders     []string    `json:"orders"`
-	Groups     []string    `json:"groups"`
-}
-
-// Column 字段
-type Column struct {
-	Field       string `json:"field"`
-	Alias       string `json:"alias"`
-	Description string `json:"description"`
-	Handler     string `json:"handler"`
-}
-
-// Condition 条件
-type Condition struct {
-	Field   string `json:"field"`
-	Handler string `json:"handler"`
-}
-
-// Join 链接参数
-type Join struct {
-	LinkField      string `json:"link_field"`
-	JoinTableName  string `json:"join_table_name"`
-	JoinTableField string `json:"join_table_field"`
-	JoinType       string `json:"join_type"`
-}
-
-// OperateSchema 通用操作schema结构体
-type OperateSchema struct{}
-
 // loadSearchSchema 查询指定路径下的json
 func loadSearchSchema(schemaName string) (SearchSchema, error) {
 	// 拼接路径
--- src/search.go
+++ src/search.go
@@ -1,7 +1,47 @@
 package easysql
 
-type SearchData struct {
-	SchemaName string `json:"schema_name"`
-	Fields     []string
-	Conditions string
+import (
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+func Search(db *gorm.DB, searchData *SearchData, schema *SearchSchema) {
+
+}
+
+func dealWhere(searchFields []string, columns []Column) (resultFields []string, handlers []FieldHandler) {
+	searchFieldStr := strings.Join(searchFields, ";")
+
+	// 判断fieldStr是否为空
+	isNull := len(searchFieldStr) == 0
+
+	for _, c := range columns {
+		if isNull || strings.Contains(searchFieldStr, c.Alias) {
+			resultFields = append(resultFields, c.Field+" AS "+c.Alias)
+		}
+
+		if (isNull || strings.Contains(searchFieldStr, c.Alias)) && len(c.Handler) > 0 {
+			handlerStructStrs := strings.Split(c.Handler, ";")
+			handlerAndMethodStrs := strings.Split(handlerStructStrs[0], ".")
+			// handlerStructStrs的长度必须大于等于2,handlerAndMethodStrs必须等于2
+			if len(handlerStructStrs) >= 2 && len(handlerAndMethodStrs) == 2 {
+				handlerName := handlerAndMethodStrs[0]
+				handlerMethodName := handlerAndMethodStrs[1]
+				handlerMethodArgs := handlerStructStrs[1:]
+				handler := FieldHandler{
+					Name:       handlerName,
+					MethodName: handlerMethodName,
+					Args:       handlerMethodArgs,
+				}
+
+				handlers = append(handlers, handler)
+			}
+		}
+	}
+	return
+}
+
+func dealJoin() {
+
 }
